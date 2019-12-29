bae953f453881c6400161f1bac6427012e7b5418
SimHongSub <oxhe2038@naver.com>
2019-12-29 18:33:24 +0900

    심홍섭 2019.12.29 sample 소스 수정(전체 train데이터 학습)

--- sample.py
+++ sample.py
@@ -18,29 +18,19 @@ from eunjeon import Mecab  # SHS-형태소분석 라이브러리 추가
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-train_data = pd.read_csv(BASE_DIR+"/data/train.csv")    # 스미싱 데이터(1) : 18703개, 스미싱 데이터 X (0) : 277242개
+# train_data = pd.read_csv(BASE_DIR+"/data/train.csv")    # 스미싱 데이터(1) : 18703개, 스미싱 데이터 X (0) : 277242개
+train_data = pd.read_csv(BASE_DIR+"/data/modified_data.csv")
 test_data = pd.read_csv(BASE_DIR+"/data/public_test.csv")
 submission_data = pd.read_csv(BASE_DIR+"/data/submission_제출양식.csv")
 
 random.seed(2019)
-train_nsm_list = list(train_data[train_data['smishing'] != 1].index)
-train_nsmishing = random.sample(train_nsm_list, 18703)    # under sampling
-train_smishing = list(train_data[train_data['smishing'] == 1].index)
+# train_nsm_list = list(train_data[train_data['smishing'] != 1].index)
+# train_nsmishing = random.sample(train_nsm_list, 18703)    # under sampling
+# train_smishing = list(train_data[train_data['smishing'] == 1].index)
 
-sampling_train_data = train_data.iloc[train_smishing + train_nsmishing, :].reset_index(drop=True)
-
-test_data['smishing'] = 2  # smishing 컬럼 추가
-
-tokenizer = Mecab()
-
-print("train data 토큰화")
-train_doc = [(tokenizer.pos(x), y) for x, y in tqdm(zip(sampling_train_data['text'], sampling_train_data['smishing']))]
-
-print("test data 토큰화")
-test_doc = [(tokenizer.pos(x), y) for x, y in tqdm(zip(test_data['text'], test_data['smishing']))]
-
+train_list = list(train_data['smishing'].index)
+selected_list = []
 stopwords = ['XXX', '.', '을', '를', '이', '가', '-', '(', ')', ':', '!', '?', ')-', '.-', 'ㅡ', 'XXXXXX', '..', '.(', '은', '는']  # 필요없는 단어 리스트
-
 
 def get_couple(_words):
     global stopwords
@@ -53,40 +43,54 @@ def get_couple(_words):
         #     print(_words[i + 1][0])
         yield _words[i][0], _words[i + 1][0]
 
+while len(train_list) > 0:
 
-X_train = []  # text 값 리스트
-Y_train = []  # 각 text에 대한 smishing 값 리스트
+    if len(train_list) > 37403:
+        selected_list = random.sample(train_list, 37403)
+        train_list = [index for index in train_list if index not in selected_list]
+    else:
+        selected_list = random.sample(train_list, len(train_list))
+        train_list = [index for index in train_list if index not in selected_list]
 
-print("train data 토큰 필요없는 단어리스트 제거, 모형에 사용하기 위한 데이터 전처리")
-j = 0
-for lwords in train_doc:
-    if j == 0:
-        print(lwords)
-        print(lwords[0])
-        print(lwords[1])
-        j += 1
+# sampling_train_data = train_data.iloc[train_smishing + train_nsmishing, :].reset_index(drop=True)
 
-    Y_train.append(lwords[1])
+    sampling_train_data = train_data.iloc[selected_list, :].reset_index(drop=True)
 
-    temp = []
-    for x, y in get_couple(lwords[0]):
-        temp.append("{}.{}".format(x, y))
-        if j == 1:
-            print("temp : ")
-            print(temp)
-            j += 1
+    test_data['smishing'] = 2  # smishing 컬럼 추가
 
-    X_train.append(" ".join(temp))
-    if j == 2:
-        print(temp)
-        print(" ".join(temp))
-        print("X_train : ")
-        print(X_train)
-        j += 1
+    tokenizer = Mecab()
 
-X_test = []  # test데이터 text 값 리스트
+    print("train data 토큰화")
+    train_doc = [(tokenizer.pos(x), y) for x, y in tqdm(zip(sampling_train_data['text'], sampling_train_data['smishing']))]
+
+
+    X_train = []  # text 값 리스트
+    Y_train = []  # 각 text에 대한 smishing 값 리스트
+
+    print("train data 토큰 필요없는 단어리스트 제거, 모형에 사용하기 위한 데이터 전처리")
+    for lwords in train_doc:
+        Y_train.append(lwords[1])
+
+        temp = []
+        for x, y in get_couple(lwords[0]):
+            temp.append("{}.{}".format(x, y))
+
+        X_train.append(" ".join(temp))
+
+    v = CountVectorizer()
+
+    v.fit(X_train)
+
+    vec_x_train = v.transform(X_train).toarray()
+
+    m1 = MultinomialNB()
+    m1.fit(vec_x_train, Y_train)
+
+print("test data 토큰화")
+test_doc = [(tokenizer.pos(x), y) for x, y in tqdm(zip(test_data['text'], test_data['smishing']))]
 
 print("test data 토큰 필요없는 단어리스트 제거, 모형에 사용하기 위한 데이터 전처리")
+X_test = []  # test데이터 text 값 리스트
 for lwords in test_doc:
 
     temp = []
@@ -95,37 +99,11 @@ for lwords in test_doc:
 
     X_test.append(" ".join(temp))
 
-v = CountVectorizer()
 
-v.fit(X_train)
-print("countvectorizer : ")
-print(v.vocabulary_)
-
-vec_x_train = v.transform(X_train).toarray()
-print("vec_x_train : ")
-print(vec_x_train[0])
 vec_x_test = v.transform(X_test).toarray()
-
-m1 = MultinomialNB()
-m1.fit(vec_x_train, Y_train)
-
-y_train_pred1 = m1.predict_proba(vec_x_train)
-y_train_pred1_one = [i[1] for i in y_train_pred1]
 
 y_test_pred1 = m1.predict_proba(vec_x_test)
 y_test_pred1_one = [i[1] for i in y_test_pred1]
-
-print("train data 스미싱 확률")
-print(y_train_pred1)
-print(y_train_pred1_one)
-
-sampling_train_data['smishing'] = y_train_pred1_one
-
-sampling_train_data.to_csv("sample_train.csv", index=False)
-
-print("test data 스미싱 확률")
-print(y_test_pred1)
-print(y_test_pred1_one)
 
 submission_data['smishing'] = y_test_pred1_one
 
